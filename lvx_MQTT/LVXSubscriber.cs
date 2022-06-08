f7046f4be31a01d4653282e38b118605ebcc46e9
yincj123 <aufgabenyin@gmail.com>
2022-06-09 01:42:18 +0200

    change

--- lvx_MQTT/LVXSubscriber.cs
+++ lvx_MQTT/LVXSubscriber.cs
@@ -11,16 +11,20 @@ namespace lvx_MQTT
         static async Task Main(string[] args)
         {
             var mqttFactory = new MqttFactory();
+            string host = "mqtt://192.168.178.5";
             IMqttClient client = mqttFactory.CreateMqttClient();
             var option = new MqttClientOptionsBuilder()
                 .WithClientId(Guid.NewGuid().ToString())
                 //.WithClientId("test123")
-                .WithTcpServer("mqtt://192.168.178.5",1883)
+                .WithTcpServer(host,1883)
                 //.WithTcpServer("mqtt://192.168.178.5:1883")
                 //.WithTcpServer("192.168.178.5",1883)
+                //.WithWebSocketServer("mqtt://192.168.178.5:1883")
+                
                 .WithCredentials("apollo","PJn3-mktq")
                 .WithCleanSession()
                 .Build();
+            
             client.UseConnectedHandler(async e =>
             {
                 Console.WriteLine("connected");
