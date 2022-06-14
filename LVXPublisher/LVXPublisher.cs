5966c16c308bf4c1fbfc4124a50099c1756c9c5f
yincj123 <aufgabenyin@gmail.com>
2022-06-14 11:38:28 +0200

    change

---
+++ LVXPublisher/LVXPublisher.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LVXPublisher
+{
+    class LVXPublisher
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
