ada1cd388152820ae6c9e1b0f3a2c0b59d758798
yinTUB <aufgabenyin@gmail.com>
2022-08-01 17:49:35 +0200

    01,08

---
+++ Apicall/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Apicall
+{
+    class Lvx_Info
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+        
+        [JsonPropertyName("connection_device_name")]
+        public string Device_name { get; set; }
+
+        [JsonPropertyName("state")]
+        public string State { get; set; }
+
+        [JsonPropertyName("heath")]
+        public string Heath { get; set; }
+
+        [JsonPropertyName("error")]
+        public string Error { get; set; }
+
+        [JsonPropertyName("attach")]
+        public string Attach { get; set; }
+
+
+
+    }
+}
